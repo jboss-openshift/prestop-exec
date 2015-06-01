8b4f9f91983e9e18856eaf70b487211155960d6c
Ales Justin <ales.justin@gmail.com>
2015-06-01 15:38:48 +0200

    Add timeout / attempt counter.

--- prestop.go
+++ prestop.go
@@ -8,9 +8,10 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/jboss-openshift/prestop-exec/sources"
-)
+	"fmt")
 
 var argPollDuration = flag.Duration("poll_duration", 10*time.Second, "Polling duration")
+var argPollAttempts	= flag.Int("poll_attempts", 1000, "Polling attempts")
 
 func main() {
 	flag.Parse()
@@ -29,9 +30,15 @@ func doWork() error {
 	source := sources.NewSource()
 	ticker := time.NewTicker(*argPollDuration)
 	defer ticker.Stop()
+	counter := 0
 	for {
 		select {
 		case <-ticker.C:
+			if counter > *argPollAttempts {
+				return fmt.Errorf("Timed out!")
+			}
+			counter++
+
 			done, err := source.CheckProgress()
 			if err != nil {
 				return err
